d48b7e4fd5ec4306bb9c7dba021fa09c812d9857
Nate Wong <nwwong17@gmail.com>
2026-05-11 12:17:33 -0700

    touched up main menu for demo

---
+++ Assets/Scripts/StartMenuManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void QuitGame()
+    {
+        // Quit the application
+        Application.Quit();
+    }
+}
