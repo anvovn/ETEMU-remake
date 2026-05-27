bbde94e95c3fd80ba50ef059b0c2091b5ce143cc
Nate Wong <nwwong17@gmail.com>
2026-05-27 02:01:36 -0700

    added eric wills cube that spawns evil chairs

---
+++ Assets/Scripts/ObstacleBehaviour.cs
@@ -0,0 +1,22 @@
+
+using UnityEngine;
+
+public class ObstacleBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject obstaclePrefab;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //on start, set timer for 3 seconds. then spwan obstacle
+        Invoke(nameof(SpawnObstacle), 3f);
+
+    }
+    void SpawnObstacle()
+    {
+        Vector3 spawnPosition = new Vector3(transform.position.x, 1f, transform.position.z);
+        Instantiate(obstaclePrefab, spawnPosition, Quaternion.Euler(270, 0, 0));
+        Destroy(gameObject);
+    }
+
+
+}
